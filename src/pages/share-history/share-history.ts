4f97ce0a0c842336f15cb156b898921300cd7c52
Rohan <rohan.ambhore@pfizer.com>
2017-11-08 19:17:14 +0530

    Share history UI

--- src/pages/share-history/share-history.ts
+++ src/pages/share-history/share-history.ts
@@ -17,6 +17,17 @@ import { UserInfoProvider } from '../../providers/user-info/user-info';
 export class ShareHistoryPage {
   isPartnerSelected: boolean = false;
   isDoctorSelected: boolean = false;
+
+  range: boolean = false;
+  month: boolean = false;
+  cycle: boolean = false;
+
+  rangeStart: string;
+  rangeEnd: string;
+  monthValue: string;
+  cycleValue: string;
+
+
   partnerEmail: string;
   doctorEmail: string;
 
@@ -29,9 +40,45 @@ export class ShareHistoryPage {
     this.isDoctorSelected = !this.isDoctorSelected;
     this.doctorEmail = this.userInfo.getDoctorEmail();
   }
-  
   togglePartner() {
     this.isPartnerSelected = !this.isPartnerSelected;
     this.partnerEmail = this.userInfo.getPartnerEmail();
   }
+
+  changeSelection(ev, i) {
+    
+    switch (i) {
+      case "RANGE":
+        this.month = false;
+        this.cycle = false;
+        break;
+
+      case "MONTH":
+        this.range = false;
+        this.cycle = false;
+        break;
+
+      case "CYCLE":
+        this.month = false;
+        this.range = false;
+        break;
+    }
+    console.log(this.rangeEnd)
+    console.log(this.rangeStart)
+    console.log(this.monthValue)
+    console.log(this.cycleValue)
+
+  }
+
+  /*
+  *  ion-date change doesn't changes ngModel when used for only month,
+  *  Hence this is a work around for it
+  */
+  monthChange(ev){
+    console.log(ev);
+    this.monthValue = ev.month;
+    console.log(this.monthValue)
+  }
+
+
 }
